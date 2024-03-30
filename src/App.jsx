d090491a7101bb34e1faae172a201e67f664f38d
Md. Mehedi Hassan <142384885+mehedihassan3@users.noreply.github.com>
2024-03-30 22:50:17 +0600

    Setup JSON sever

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from '../src/assets/react.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
