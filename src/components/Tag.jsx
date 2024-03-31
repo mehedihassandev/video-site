cd74f89ea0999824f93851b9dd73b9a7a5371f85
Md. Mehedi Hassan <142384885+mehedihassan3@users.noreply.github.com>
2024-03-31 15:11:43 +0600

    Apply tags features

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,11 +1,16 @@
-export const Tag = () => {
+import PropTypes from 'prop-types'
+export const Tag = ({ title }) => {
   return (
     <div
       className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer"
     >
-      react
+      {title}
     </div>
   )
+}
+
+Tag.prototype = {
+  title: PropTypes.string,
 }
 
 export default Tag
--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,15 +1,27 @@
-import { Tag } from "./Tag"
+import { useDispatch, useSelector } from "react-redux";
+import { Tag } from "./Tag";
+import { useEffect } from "react";
+import { fetchTags } from "../features/tags/tagsSlice";
 
 export const Tags = () => {
-  return (
+  const { tags } = useSelector((state) => state.tags);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTags());
+  }, [dispatch]);
+
+
+  return tags.lenghts > 0 ? (
     <section>
-      <div
-        className="max-w-screen-2xl mx-auto px-5 py-4 lg:px-0 flex gap-2 overflow-y-auto"
-      >
-        <Tag />
+      <div className="max-w-screen-2xl mx-auto px-5 py-4 lg:px-0 flex gap-2 overflow-y-auto">
+        {tags.map((tag) => (
+          <Tag key={tag.id} title={tag.title} />
+        ))}
       </div>
     </section>
-  )
-}
+  ) : null;
+};
 
-export default Tags
+export default Tags;
